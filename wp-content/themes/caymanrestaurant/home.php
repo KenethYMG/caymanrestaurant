7cabca059c140bc0e493ee2aef74d769033cefeb
Jonathan <jonathan.motta@yellomg.com>
2021-08-25 10:31:26 -0600

    se removio los div ejemplo

--- wp-content/themes/caymanrestaurant/home.php
+++ wp-content/themes/caymanrestaurant/home.php
@@ -3,8 +3,4 @@
 <h2> Cayman Restaurant </h2>
 <h3> Cayman Restaurant </h3>
 <h4> Cayman Restaurant </h4>
-<div class="ejemplo"></div>
-<div class="ejemplo"></div>
-<div class="ejemplo"></div>
-<div class="ejemplo"></div>
 <?php get_footer(); ?>

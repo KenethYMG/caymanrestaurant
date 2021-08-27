b83823b75b0370630e12067fc26af017b7304fa4
Keneth <keneth.rosales@yellomg.com>
2021-08-27 17:16:49 -0600

    Carousel Advances

--- wp-content/themes/caymanrestaurant/home/section-carousel.php
+++ wp-content/themes/caymanrestaurant/home/section-carousel.php
@@ -1,9 +1,14 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
-            <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="col-md-12 px-0">
+            <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="">
                 <?php query_posts('post_type=slider&post_per_page=1') ?>
                 <div class="carousel-inner">
+                    <div class="test-index position-absolute">
+                         <h1 class="title-home font-white">
+                                Example
+                         </h1>
+                     </div>
                     <?php 
                     while (have_posts()): the_post(); 
                         $images_url = get_post_meta( get_the_ID(), 'upload-images', true );
@@ -24,15 +29,9 @@
                                     <img src="<?php echo $image; ?>" class="d-block w-100">
                                 </div>
                      <?php } ?>
-                     <?php endwhile; ?>
-                     
+                     <?php endwhile; ?>  
                 </div>
             </div>
         </div>
     </div>
 </div>
-<style>
-.carousel-inner{
-    height: 600px;
-}
-</style>

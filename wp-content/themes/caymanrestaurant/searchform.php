45e1b7f651f19a5c6da45bc5fd6cd8b0168ea291
Jonathan <jonathan.motta@yellomg.com>
2021-09-08 16:33:26 -0600

    Auto stash before merge of "Master" and "origin/Master"

--- wp-content/themes/caymanrestaurant/searchform.php
+++ wp-content/themes/caymanrestaurant/searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo HOMELINK ?>">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
     <div>
         <input type="text" value="" name="s" id="s" class="text-uppercase" placeholder="SEARCH..." />
         <div class="submit_box">
